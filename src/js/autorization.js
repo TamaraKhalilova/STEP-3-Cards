9f19edb35efdab46b2d437df79f394863ca92b08
Tamara Onufriieva <tamara.halilova1@gmail.com>
2020-08-06 20:52:14 +0300

    add final autorization

--- src/js/autorization.js
+++ src/js/autorization.js
@@ -1,25 +1,22 @@
+const pageWrapper = document.querySelector('.page__wrapper');
 const formAutorization = document.querySelector('.autorization');
 const btnHeader = document.querySelector('.btn--header');
-const pageWrapper = document.querySelector('.page__wrapper');
 
-btnHeader.addEventListener('click', (e)=>{
+btnHeader.addEventListener('click', ()=>{
     formAutorization.style.display = 'block';
     pageWrapper.style.filter = 'blur(5px)';
 })
 
-formAutorization.addEventListener('submit', (e)=>{
+formAutorization.addEventListener('submit', async (e)=>{
     e.preventDefault();
-    axios.post('http://cards.danit.com.ua/login',{
-        "email": "tamara.halilova1@gmail.com",
-        "password": "Kirivtazodisldi1",
-        headers: {
-            'Authorization': `Bearer ae90414be9c8` 
-          }
-    }).then(res=>{
-        if (res.data.status === 'Success'){
-            formAutorization.style.display = 'none';
-            pageWrapper.style.filter = '';
-            btnHeader.textContent = 'Создать визит'
-        }
-    })
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const response = await axios.post('http://cards.danit.com.ua/login',{email, password});
+
+    if (response.data.status === 'Success'){
+        formAutorization.style.display = 'none';
+        pageWrapper.style.filter = '';
+        btnHeader.textContent = 'Создать визит'
+    }
 })
